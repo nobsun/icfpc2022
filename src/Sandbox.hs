2c51daa8368ef36444ef566008db3f849efc1b6f
cutsea110 <cutsea110@gmail.com>
2022-09-03 01:22:29 +0900

    define cata over Block

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -6,24 +6,53 @@ import Data.Word
 
 type BlockID = [Int]
 
+-- | 簡易版
+data Color = White | Black | Red deriving (Show, Eq)
+{-
 data Color
   = RGBA { r :: Word8
          , g :: Word8
          , b :: Word8
          , a :: Word8
          } deriving (Show, Eq)
+-}
 
-type Shape = Int
+-- | NOTE: Shape は不明
+type Width  = Int
+type Height = Int
+type Shape = (Width, Height)
 
-data SimpleBlock = SimpleBlock BlockID Shape Color
-  deriving (Show, Eq)
+data SimpleBlock  = SimpleBlock BlockID Shape Color deriving (Show, Eq)
 
-data ComplexBlock = ComplexBlock Shape ChildBlocks
-  deriving (Show, Eq)
+data Block = Leaf SimpleBlock       -- ^ SimpleBlock
+           | Node (Shape, [Block])  -- ^ ComplexBlock
+           deriving (Show, Eq)
 
-type ChildBlocks = [Block]
-
-type Block = Either SimpleBlock ComplexBlock
+-- | NOTE: Node ケースで rhs の式における s の置き場所は Shape の意味によっては変える可能性あり
+--
+--                  [leaf, node]
+--          T  <---------------- A + [T]
+--          |                      |
+--  (|f, g|)|                      | 1_A + map (|f, g|)
+--          |                      |
+--          |                      |
+--          X  <---------------- A + [X]
+--                  [f, g]
+--        
+-- >>> let b = Leaf (SimpleBlock [0] (400, 400) White)
+-- >>> cataBlock Leaf Node b == b
+-- >>> True
+--
+-- >>> let l = Leaf (SimpleBlock [0, 0] (200, 400) White)
+-- >>> let r = Leaf (SimpleBlock [0, 1] (200, 400) White)
+-- >>> let b = Node ((400, 400), [l, r])
+-- >>> cataBlock Leaf Node b == b
+-- >>> True
+cataBlock :: (SimpleBlock -> a) -> ((Shape, [a]) -> a) -> Block -> a
+cataBlock f g = u
+  where
+    u (Leaf b)       = f b
+    u (Node (s, bs)) = g (s, map u bs)
 
 -- | moves
 
