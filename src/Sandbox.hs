77237281fabb82cffa6c48e46f7a7bb18adee0a1
cutsea110 <cutsea110@gmail.com>
2022-09-02 23:32:20 +0900

    define static model

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,5 +1,25 @@
-module Sandbox ( test ) where
+module Sandbox where
 
--- | cutsea's sanbox
-test :: String
-test = "Cutsea's sandbox"
+import Data.Word
+
+type BlockID = [Int]
+
+
+data Color
+  = RGBA { r :: Word8
+         , g :: Word8
+         , b :: Word8
+         , a :: Word8
+         } deriving (Show, Eq)
+
+type Shape = Int
+
+data SimpleBlock = SimpleBlock Shape Color
+  deriving (Show, Eq)
+
+data ComplexBlock = ComplexBlock Shape ChildBlocks
+  deriving (Show, Eq)
+
+type ChildBlocks = [Block]
+
+type Block = Either SimpleBlock ComplexBlock
