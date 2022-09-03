db46d4b5820b37c11de455dcb4e03c964aa6951c
Masahiro Sakai <masahiro.sakai@gmail.com>
2022-09-03 23:43:43 +0900

    update comment

--- src/EvalJuicyPixels.hs
+++ src/EvalJuicyPixels.hs
@@ -41,7 +41,7 @@ evalMove :: PrimMonad m => Move -> M m ()
 evalMove (COLOR bid (r,g,b,a)) = do
   ((x,y), (w,h)) <- lookupBlock bid
   img <- ask
-  -- TODO: αチャンネルを考慮して色を混ぜる必要がある?w
+  -- αチャンネルを考慮して色を混ぜる必要はないようだ
   let px = PixelRGBA8 (fromIntegral r) (fromIntegral g) (fromIntegral b) (fromIntegral a)
   forM_ [y..y+h-1] $ \y' -> do
     forM_ [x..x+w-1] $ \x' -> do
