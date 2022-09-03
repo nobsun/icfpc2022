f3cac574d0f6edc19d5a4b222f688f6415661773
Kei Hibino <ex8k.hibino@gmail.com>
2022-09-03 16:36:32 +0900

    http-api-script: add submit script

---
+++ api/submit.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+. api/lib
+set_envs
+
+usage() {
+    cat <<EOF
+Usage: $0 PROBLEM_ID FILENAME
+EOF
+}
+
+if [ x"$1" = x ]; then
+    usage
+    exit 0
+fi
+
+prob_id="$1"
+shift
+
+if [ x"$1" = x ]; then
+    usage
+    exit 0
+fi
+
+file="$1"
+shift
+
+api_request_nl -X POST -F file=@"${file}" ${api_prefix}api/submissions/${prob_id}/create
