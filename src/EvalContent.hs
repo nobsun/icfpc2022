8e720c6c3c8b95ccc3ec4b24c70103b9059821c5
Masahiro Sakai <masahiro.sakai@gmail.com>
2022-09-04 15:21:02 +0900

    simplify

--- src/EvalContent.hs
+++ src/EvalContent.hs
@@ -26,7 +26,7 @@ import Content
 data CanvasState
   = CanvasState
   { canvasCounter :: !Int
-  , canvasBlocks :: Map.Map BlockId (Shape, Content)
+  , canvasBlocks :: Map.Map BlockId (Point, Content)
   }
 
 
@@ -51,7 +51,7 @@ initialState config =
   { canvasCounter = length (icBlocks config) - 1
   , canvasBlocks =
       Map.fromList
-      [ (V.singleton (read (icbBlockId block)), (Rectangle (icbBottomLeft block) (icbTopRight block), Fill (x1-x0) (y1-y0) (icbColor block)))
+      [ (V.singleton (read (icbBlockId block)), (icbBottomLeft block, Fill (x1-x0) (y1-y0) (icbColor block)))
       | block <- icBlocks config
       , let (x0,y0) = icbBottomLeft block
       , let (x1,y1) = icbTopRight block
@@ -59,10 +59,10 @@ initialState config =
   }
 
 
-renderBlocks :: Int -> Int -> Map.Map BlockId (Shape, Content) -> Image PixelRGBA8
+renderBlocks :: Int -> Int -> Map.Map BlockId (Point, Content) -> Image PixelRGBA8
 renderBlocks w h blocks = runST $ do
   img <- createMutableImage w h (PixelRGBA8 255 255 255 255)
-  forM_ (Map.elems blocks) $ \(Rectangle (x,y) _, content) -> renderContentM img x y content
+  forM_ (Map.elems blocks) $ \((x,y), content) -> renderContentM img x y content
   unsafeFreezeImage img  
 
 
@@ -76,85 +76,88 @@ evalMove w h move s = do
 
 evalMoveM :: Move -> M ()
 evalMoveM (COLOR bid color) = do
-  (shape, content) <- lookupBlock bid
+  (pos, content) <- lookupBlock bid
   -- αチャンネルを考慮して色を混ぜる必要はないようだ
-  modifyBlocks $ Map.insert bid (shape, Fill (contentWidth content) (contentHeight content) color)
+  modifyBlocks $ Map.insert bid (pos, Fill (contentWidth content) (contentHeight content) color)
   canvasSize <- getCanvasSize
-  addCost $ 5 * canvasSize / fromIntegral (shapeSize shape)
+  addCost $ 5 * canvasSize / fromIntegral (contentSize content)
 evalMoveM move@(LCUT bid orientation offset) = do
-  (shape@(Rectangle (x,y) (x1,y1)), content) <- lookupBlock bid
+  ((x,y), content) <- lookupBlock bid
   modifyBlocks $ Map.delete bid
   case orientation of
     X -> do
-      unless (x <= offset && offset <= x1) $
-        throwError ("invalid move (" ++ dispMove move ++ "): " ++ show offset ++ " not in " ++ show x ++ ".." ++ show x1)
+      unless (x <= offset && offset <= x + contentWidth content) $
+        throwError ("invalid move (" ++ dispMove move ++ "): " ++ show offset ++ " not in " ++ show x ++ ".." ++ show (x + contentWidth content))
       case hsplit (offset - x) content of
         (l, r) -> do
-          modifyBlocks $ Map.insert (V.snoc bid 0) (Rectangle (x,y) (offset,y1), l)
-          modifyBlocks $ Map.insert (V.snoc bid 1) (Rectangle (offset,y) (x1,y1), r)
+          modifyBlocks $ Map.insert (V.snoc bid 0) ((x,y), l)
+          modifyBlocks $ Map.insert (V.snoc bid 1) ((offset,y), r)
     Y -> do
-      unless (y <= offset && offset <= y1) $
-        throwError ("invalid move (" ++ dispMove move ++ "): " ++ show offset ++ " not in " ++ show y ++ ".." ++ show y1)
+      unless (y <= offset && offset <= y + contentHeight content) $
+        throwError ("invalid move (" ++ dispMove move ++ "): " ++ show offset ++ " not in " ++ show y ++ ".." ++ show (y + contentHeight content))
       case vsplit (offset - y) content of
         (b, t) -> do
-          modifyBlocks $ Map.insert (V.snoc bid 0) (Rectangle (x,y) (x1,offset), b)
-          modifyBlocks $ Map.insert (V.snoc bid 1) (Rectangle (x,offset) (x1,y1), t)
+          modifyBlocks $ Map.insert (V.snoc bid 0) ((x,y), b)
+          modifyBlocks $ Map.insert (V.snoc bid 1) ((x,offset), t)
   canvasSize <- getCanvasSize
-  addCost $ 7 * canvasSize / fromIntegral (shapeSize shape)
+  addCost $ 7 * canvasSize / fromIntegral (contentSize content)
 evalMoveM move@(PCUT bid (x1,y1)) = do
-  (shape@(Rectangle (x0,y0) (x2,y2)), content) <- lookupBlock bid
+  ((x0,y0), content) <- lookupBlock bid
+  let (x2,y2) = (x0 + contentWidth content, y0 + contentHeight content)
   modifyBlocks $ Map.delete bid
   unless (x0 <= x1 && x1 <= x2 && y0 <= y1 && y1 <= y2) $
-    throwError ("invalid move (" ++ dispMove move ++ "): " ++ show (x1,y1) ++ " not in " ++ show shape)
+    throwError ("invalid move (" ++ dispMove move ++ "): " ++ show (x1,y1) ++ " not in " ++ show ((x0,y0),(x2,y2)))
   case vsplit (y1 - y0) content of
     (b, t) ->
       case (hsplit (x1 - x0) b, hsplit (x1 - x0) t) of
         ((bl,br), (tl,tr)) ->
           modifyBlocks $
-            Map.insert (V.snoc bid 0) (Rectangle (x0, y0) (x1, y1), bl) .
-            Map.insert (V.snoc bid 1) (Rectangle (x1, y0) (x2, y1), br) .
-            Map.insert (V.snoc bid 2) (Rectangle (x1, y1) (x2, y2), tr) .
-            Map.insert (V.snoc bid 3) (Rectangle (x0, y1) (x1, y2), tl)
+            Map.insert (V.snoc bid 0) ((x0, y0), bl) .
+            Map.insert (V.snoc bid 1) ((x1, y0), br) .
+            Map.insert (V.snoc bid 2) ((x1, y1), tr) .
+            Map.insert (V.snoc bid 3) ((x0, y1), tl)
   canvasSize <- getCanvasSize
-  addCost $ 10 * canvasSize / fromIntegral (shapeSize shape)
+  addCost $ 10 * canvasSize / fromIntegral (contentSize content)
 evalMoveM move@(SWAP bid1 bid2) = do
-  CanvasState cnt blocks <- get
-  block1@(shape1, content1) <- lookupBlock bid1
-  block2@(shape2, content2) <- lookupBlock bid2
-  unless ((shapeWidth shape1, shapeHeight shape1) == (shapeWidth shape2, shapeHeight shape2)) $
-    throwError ("invalid move (" ++ dispMove move ++ "): " ++ show (shapeWidth shape1, shapeHeight shape1) ++ " /= " ++ show (shapeWidth shape2, shapeHeight shape2))
+  -- CanvasState cnt blocks <- get
+  block1@(pos1, content1) <- lookupBlock bid1
+  block2@(pos2, content2) <- lookupBlock bid2
+  unless ((contentWidth content1, contentHeight content1) == (contentWidth content2, contentHeight content2)) $
+    throwError ("invalid move (" ++ dispMove move ++ "): " ++ show (contentWidth content1, contentHeight content1) ++ " /= " ++ show (contentWidth content2, contentHeight content2))
   modifyBlocks $
     Map.insert bid1 block2 .
     Map.insert bid2 block1
   canvasSize <- getCanvasSize
-  addCost $ 3 * canvasSize / fromIntegral (shapeSize shape1)
+  addCost $ 3 * canvasSize / fromIntegral (contentSize content1)
 evalMoveM move@(MERGE bid1 bid2) = do
   CanvasState cnt blocks <- get
-  (shape1@(Rectangle (x1,y1) (x1',y1')), content1) <- lookupBlock bid1
-  (shape2@(Rectangle (x2,y2) (x2',y2')), content2) <- lookupBlock bid2
+  ((x1,y1), content1) <- lookupBlock bid1
+  ((x2,y2), content2) <- lookupBlock bid2
+  let (x1',y1') = (x1 + contentWidth content1, y1 + contentHeight content1)
+      (x2',y2') = (x2 + contentWidth content2, y2 + contentHeight content2)
   let blocks' = Map.delete bid1 $ Map.delete bid2 blocks
       cnt' = cnt + 1
       bid3 = V.singleton cnt'
   put (CanvasState cnt' blocks')
-  if x1 == x2 && shapeWidth shape1 == shapeWidth shape2 && (y1' == y2 || y2' == y1) then do
-    modifyBlocks $ Map.insert bid3 (Rectangle (x1, min y1 y2) (x1', max y1' y2'), if y1 < y2 then vmerge content1 content2 else vmerge content2 content1)
-  else if y1 == y2 && shapeHeight shape1 == shapeHeight shape2 && (x1' == x2 || x2' == x1) then do
-    modifyBlocks $ Map.insert bid3 (Rectangle (min x1 x2, y1) (max x1' x2', y1'), if x1 < x2 then hmerge content1 content2 else hmerge content2 content1)
+  if x1 == x2 && contentWidth content1 == contentWidth content2 && (y1' == y2 || y2' == y1) then do
+    modifyBlocks $ Map.insert bid3 ((x1, min y1 y2), if y1 < y2 then vmerge content1 content2 else vmerge content2 content1)
+  else if y1 == y2 && contentHeight content1 == contentHeight content2 && (x1' == x2 || x2' == x1) then do
+    modifyBlocks $ Map.insert bid3 ((min x1 x2, y1), if x1 < x2 then hmerge content1 content2 else hmerge content2 content1)
   else do
     throwError ("invalid move (" ++ dispMove move ++ ")")
   canvasSize <- getCanvasSize
-  addCost $ 1 * canvasSize / fromIntegral (max (shapeSize shape1) (shapeSize shape2))
+  addCost $ 1 * canvasSize / fromIntegral (max (contentSize content1) (contentSize content2))
 
 
-lookupBlock :: BlockId -> M (Shape, Content)
+lookupBlock :: BlockId -> M (Point, Content)
 lookupBlock bid = do
   CanvasState _ blocks <- get
   case Map.lookup bid blocks of
     Nothing -> throwError ("no such block: " ++ dispBlockId bid)
-    Just rect -> return rect
+    Just x -> return x
 
 
-modifyBlocks :: (Map.Map BlockId (Shape, Content) -> Map.Map BlockId (Shape, Content)) -> M ()
+modifyBlocks :: (Map.Map BlockId (Point, Content) -> Map.Map BlockId (Point, Content)) -> M ()
 modifyBlocks f = modify (\canvas -> canvas{ canvasBlocks = f (canvasBlocks canvas) })
 
 
