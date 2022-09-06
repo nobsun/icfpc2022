244c1d7e7455ef19af2cc1fc68d4677cc98b03f0
Nobuo Yamashita <nobsun@sampou.org>
2022-09-06 11:40:04 +0900

    slightly change pngview

--- app/gloss/pngview/Main.hs
+++ app/gloss/pngview/Main.hs
@@ -11,9 +11,9 @@ main = do
     ; case args of
         []  -> putStrLn "pngview <png-file>"
         a:_ -> do { img <- Gloss.loadJuicyPNG a
-                  ; Gloss.display window Gloss.black (fromMaybe Gloss.blank img)
+                  ; Gloss.display (window a) Gloss.black (fromMaybe Gloss.blank img)
                   }
     }
 
-window :: Gloss.Display
-window = Gloss.InWindow "ICFP 2022" (960, 960) (400,400)
+window :: String -> Gloss.Display
+window t = Gloss.InWindow t (960, 960) (400,400)
